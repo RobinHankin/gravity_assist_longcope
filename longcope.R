c375ccc56b8d20185e6348c3a287917f707cddce
RobinHankin <hankin.robin@gmail.com>
2020-07-19 08:47:23 +1200

    slight visual edits

--- longcope.R
+++ longcope.R
@@ -8,7 +8,7 @@
     levels_peri <- unique(sort(c(0.01*(1:10),0.1*(1:9),0.9+(1:9)/100))) 
 
     ## and now the aphelion:
-    levels_ap <- unique(sort(c(1 + (1:10)/100,1+(1:10)/10,3:10,30)))  # aphelion
+    levels_ap <- unique(sort(c(1 + (1:10)/100,1+(1:10)/10,3:10,30)))
 
     ## range for vx == v_phi and vy==v_r:
     vx <- seq(from=0,to=sqrt(2),len=n)
@@ -18,7 +18,12 @@
     perihelion <- apply(v,1,f_peri) 
     aphelion <- apply(v,1,f_ap  )
     ## plot commands start
-    plot(0:1,xlim=c(0,sqrt(2)),ylim=c(0,sqrt(2)),type = "n",xlab=expression(v[phi]),ylab=expression(v[r]),axes=FALSE,main="Longcope velocity-space map of Keplerian orbits",asp=1)
+    plot(0:1, type = "n", asp=1, axes=FALSE,
+         xlim=c(0,sqrt(2)),ylim=c(0,sqrt(2)),
+         xlab=expression(v[phi]),ylab=expression(v[r]),
+         main="Longcope velocity-space map of Keplerian orbits"
+         )
+
     axis(1,pos=0,at=seq(from=0,by=0.2,to=1.4))
     axis(2,pos=0)
     contour(vx,vy,matrix(aphelion,n,n), levels=levels_ap,add=TRUE,lwd=0.5)
@@ -34,4 +39,4 @@
            col=c("red","black","blue","green"),
            legend=c("perihelion","aphelion","scattering","escape"))
     
-}
+}  # function longcope() ends
